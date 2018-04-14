795eeb86711ecb72d1b9115ac114f3cb624a2512
Modular <30032008+ModularDeception@users.noreply.github.com>
2018-04-14 16:02:28 -0400

    Create Calculator.js

---
+++ Code/Calculator.js
@@ -0,0 +1,21 @@
+function multiplication(p1, p2) {
+    return p1 * p2;
+}
+
+function subtraction(p1, p2) {
+    return p1 - p2;
+}
+
+function addition(p1, p2) {
+    return p1 + p2;
+}
+
+function division(p1, p2) {
+    return p1 / p2;
+}
+
+
+
+var x = subtraction(4, 2);    
+
+print (x)
